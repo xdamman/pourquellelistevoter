6bac2bd88cf15d3bd163281c44dfd1a2da09570c
Xavier Damman <xdamman@gmail.com>
2018-10-07 17:50:40 +0200

    added climate first lists

--- src/components/ListWarnings.js
+++ src/components/ListWarnings.js
@@ -18,6 +18,9 @@ class ListWarnings extends React.Component {
         <p>✅ Cette liste milite avant tout pour un nouveau processus démocratique pour impliquer le citoyen dans les décisions politiques.<br />
           En votant pour cette liste, vous ne devrez pas attendre 2024 ou descendre dans la rue pour faire entendre votre voix!</p>
         }
+        { list.info.climate_first &&
+        <p>✅ Cette liste milite avant tout pour prendre des mesures contre le réchauffement climatique 🌍 🌱</p>
+        }
         { list.info.year_established < 2000 &&
         <p>⚠️ Cette liste émane d'un parti politique du {Math.ceil(list.info.year_established/100)}e siècle. Il a été créé en {list.info.year_established} (il y a {year - list.info.year_established} ans!).<br />
         A ce moment, il n'y avait pas d'Internet. Ce n'est donc pas dans leur ADN de partager l'information et de permettre à tout le monde de collaborer.</p>
